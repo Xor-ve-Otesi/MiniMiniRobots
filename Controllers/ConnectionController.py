d8d1d783ec417bc7214e8d3485dc819d45a5e086
YusufOnatYilmaz <yusufonatyilmaz@gmail.com>
2023-04-26 17:45:59 +0300

    connection

--- Controllers/ConnectionController.py
+++ Controllers/ConnectionController.py
@@ -1,3 +1,17 @@
+import socket
+import network
+
 class ConnectionController():
-    def __init__(self) -> None:
-        raise NotImplementedError
+    def __init__(self, ssid, password) -> None:
+        self.ssid = ssid
+        self.password = password
+
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+        wlan.connect(self.ssid, self.password)
+
+        ai = socket.getaddrinfo("192.168.184.141", 1242) # Address of Web Server
+        addr = ai[0][-1]
+        self.cl = socket.socket() # Open socket
+        self.cl.connect(addr)
+        #"""
